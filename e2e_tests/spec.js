21d4b847ee723d5b0465ee33b01df2af02902deb
valeria20 <sholomova96@gmail.com>
2017-03-26 21:07:24 +0300

    .

--- e2e_tests/spec.js
+++ e2e_tests/spec.js
@@ -1,5 +1,5 @@
-var MainPage=requre('./PageObject/MainPage.js');
-var AdvancedFilters=requre('./PageObject/AdvancedFilters.js');
+var MainPage=require('./PageObject/MainPage.js');
+var AdvancedFilters=require('./PageObject/AdvancedFilters.js');
 describe('test filter of www.upwork.com', function(){
     it('dearch for russian web developers', function(){
         var mainPage=new PageObject.MainPage();
