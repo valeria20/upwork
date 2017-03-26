4b44d02605d6561cd865e37b0359ba0db39ec8ce
valeria20 <sholomova96@gmail.com>
2017-03-26 19:19:53 +0300

    PageObject is modified

--- e2e_tests/spec.js
+++ e2e_tests/spec.js
@@ -1,5 +1,6 @@
 var MainPage=requre('./PageObject/MainPage.js');
 var AdvancedFilters=requre('./PageObject/AdvancedFilters');
+var PageObject = require('require.all')('./PageObject');
 describe('test filter of www.upwork.com', function(){
     it('dearch for russian web developers', function(){
         var mainPage=new PageObject.MainPage();
