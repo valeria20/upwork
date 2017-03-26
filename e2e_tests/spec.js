13fe96d58fb4f234e45f3162808b7a65a9c31c87
valeria20 <sholomova96@gmail.com>
2017-03-26 22:15:40 +0300

    spec is modified

--- e2e_tests/spec.js
+++ e2e_tests/spec.js
@@ -1,29 +1,22 @@
-var MainPage=require('../PageObject/MainPage.js');
-var AdvancedFilters=require('../PageObject/AdvancedFilters.js');
-describe('test filter of www.upwork.com', function(){
-it('dearch for russian web developers', function(){
-var mainPage=new MainPage();
-var andvancedFilters=new AndvancedFilters();
-var searchedFreelancers="Web Developers";
-var searchedLocation="Russia";
-var expectedLocation="Russia";
+var MainPage = require('../pageObjects/MainPage.js');
+var AdvancedFilters = require('../pageObjects/AdvancedFilters.js');
+describe('test filter of www.upwork.com', function () {
+var mainPage = new MainPage();
+var advancedFilters = new AdvancedFilters();
+it('dearch for russian web developers', function () {
+
+var searchedFreelancers = "Web Developers";
+var searchedLocation = "Russia";
+var expectedLocation = "Russia";
 mainPage.get();
-/**
-* input to 'Find Freelancers' field 'Web Developers'
-* @param {string} searchedFreelancers - freelancers we should find
-*/
+
 mainPage.findAllWebDevelopers(searchedFreelancers);
 advancedFilters.goToAdvancedfilters();
-/**
-* search russian Web Developers
-* @param {string} searchedLocation - country where web developers are located
-*/
+
 advancedFilters.searchRussianWebDevelopers(searchedLocation);
-/**
-* list of location elements
-*/
-var locations=element.all(by.xpath(".//*[@id='oContractorResults']//div[@class='no-wrap ellipsis']"));
-for(realLocation in locations){
+
+var locations = element.all(by.xpath(".//*[@id='oContractorResults']//div[@class='no-wrap ellipsis']"));
+for (realLocation in locations) {
 expect(realLocation.getText()).ToEqual(expectedLocation);
 }
 });
