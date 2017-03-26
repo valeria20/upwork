260ff4c7ffcd797f6404aa702889b6c6786123d1
valeria20 <sholomova96@gmail.com>
2017-03-26 21:40:17 +0300

    .

--- e2e_tests/spec.js
+++ e2e_tests/spec.js
@@ -1,30 +1,30 @@
 var MainPage=require('../e2e_tests/PageObject/MainPage.js');
 var AdvancedFilters=require('../e2e_tests/PageObject/AdvancedFilters.js');
 describe('test filter of www.upwork.com', function(){
-    it('dearch for russian web developers', function(){
-        var mainPage=new PageObject.MainPage();
-        var andvancedFilters=new PageObject.AndvancedFilters();
-        var searchedFreelancers="Web Developers";
-        var searchedLocation="Russia";
-		var expectedLocation="Russia";
-        mainPage.get();
-        /**
-         * input to 'Find Freelancers' field 'Web Developers'
-         * @param {string} searchedFreelancers - freelancers we should find
-         */
-        mainPage.findAllWebDevelopers(searchedFreelancers);
-        advancedFilters.goToAdvancedfilters();
-        /**
-         * search russian Web Developers
-         * @param {string} searchedLocation - country where web developers are located
-         */
-        advancedFilters.searchRussianWebDevelopers(searchedLocation);
-        /**
-         * list of location elements
-         */
-        var locations=element.all(by.xpath(".//*[@id='oContractorResults']//div[@class='no-wrap ellipsis']"));                    
-        for(realLocation in locations){
-            expect(realLocation.getText()).ToEqual(expectedLocation);
-        }
-    });
+it('dearch for russian web developers', function(){
+var mainPage=new MainPage();
+var andvancedFilters=new AndvancedFilters();
+var searchedFreelancers="Web Developers";
+var searchedLocation="Russia";
+var expectedLocation="Russia";
+mainPage.get();
+/**
+* input to 'Find Freelancers' field 'Web Developers'
+* @param {string} searchedFreelancers - freelancers we should find
+*/
+mainPage.findAllWebDevelopers(searchedFreelancers);
+advancedFilters.goToAdvancedfilters();
+/**
+* search russian Web Developers
+* @param {string} searchedLocation - country where web developers are located
+*/
+advancedFilters.searchRussianWebDevelopers(searchedLocation);
+/**
+* list of location elements
+*/
+var locations=element.all(by.xpath(".//*[@id='oContractorResults']//div[@class='no-wrap ellipsis']"));
+for(realLocation in locations){
+expect(realLocation.getText()).ToEqual(expectedLocation);
+}
+});
 });
